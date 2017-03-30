7d00fcaacf5893709470567583d002ad3600b5e4
AndresBanda <andres.banda.castro@gmail.com>
2017-03-30 12:48:44 -0500

    DELETED for testing

--- catalog/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class catalogConfig(AppConfig):
-    name = 'catalog'
--- catalog/urls.py
+++
@@ -1,7 +0,0 @@
-# Use include() to add URLS from the catalog application 
-from django.conf.urls import include, url
-from . import views
-urlpatterns = [
-    #url(r'^catalog/', include('catalog.urls')),
-    url(r'^$', views.index, name='index'),
-]
